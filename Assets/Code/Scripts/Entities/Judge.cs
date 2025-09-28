0ead67ded01aea1e9ef98e0737cc6ea2bf4bfe22
Charlie Boyer <charlieboyer@outlook.fr>
2025-09-28 12:01:32 +0200

    feature(player-choices): selectable choices from SO pool is now functional

--- Assets/Code/Scripts/Entities/Judge.cs
+++ Assets/Code/Scripts/Entities/Judge.cs
@@ -1,6 +1,9 @@
-﻿using Code.Scripts.Singleton;
+﻿using System;
+using Code.Scripts.Singleton;
+using Code.Scripts.SO;
 using Code.Scripts.Types;
 using Code.Scripts.Utils;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,6 +22,31 @@ namespace Code.Scripts.Entities
         [Header("UI Module")]
         [SerializeField] private TMP_Text _nameBox;
         [SerializeField] private Slider _convictionGauge;
+        [SerializeField] private float _convictionGaugeAnimDuration;
+
+        private int _currentConviction;
+        private int Conviction
+        {
+            get => _currentConviction;
+
+            set
+            {
+                _currentConviction = Mathf.FloorToInt(value);
+                _convictionGauge.DOValue(_currentConviction, _convictionGaugeAnimDuration);
+            }
+        }
+
+        private void OnEnable()
+        {
+            GameManager.Instance.OnPlayerAction += ResolveDefenseStatement;
+        }
+
+        private void OnDisable()
+        {
+            GameManager.Instance.OnPlayerAction -= ResolveDefenseStatement;
+        }
+
+        // ----- //
 
         public static void InitializeJudges()
         {
@@ -32,11 +60,44 @@ namespace Code.Scripts.Entities
 
         public void Init()
         {
-            gameObject.name = name + "_" + _refID;
             _nameBox.text = _refID.ToString().AddSpacesToEnum();
+
             _convictionGauge.wholeNumbers = true;
             _convictionGauge.maxValue = GameManager.Instance.JudgesMaxConviction;
             _convictionGauge.value = 0;
+        }
+
+        public void ResolveDefenseStatement(PlayerChoiceSO action)
+        {
+            int baseValue = action.ConvictionValue;
+            int totalConvictionPoints;
+
+            if (!action.Targets.HasFlag(_refID))
+                return;
+
+            switch (action.Category)
+            {
+                case PlayerChoiceCategory.Injustice:
+                    totalConvictionPoints = Mathf.RoundToInt(baseValue * _sensibilities.Injustice * _sensibilities.AffinityMultiplier);
+                    break;
+                case PlayerChoiceCategory.Attack:
+                    totalConvictionPoints = Mathf.RoundToInt(baseValue * _sensibilities.Injustice * _sensibilities.AffinityMultiplier);
+                    break;
+                case PlayerChoiceCategory.Dishonor:
+                    totalConvictionPoints = Mathf.RoundToInt(baseValue * _sensibilities.Injustice * _sensibilities.AffinityMultiplier);
+                    break;
+                case PlayerChoiceCategory.Threat:
+                    totalConvictionPoints = Mathf.RoundToInt(baseValue * _sensibilities.Injustice * _sensibilities.AffinityMultiplier);
+                    break;
+                case PlayerChoiceCategory.Diplomacy:
+                    totalConvictionPoints = Mathf.RoundToInt(baseValue * _sensibilities.Injustice * _sensibilities.AffinityMultiplier);
+                    break;
+
+                default:
+                    throw new ArgumentException($"Error: Invalid PlayerChoiceSO category, please check SO: \"{action.name}\".");
+            }
+
+            Conviction += totalConvictionPoints;
         }
     }
 }
--- Assets/Code/Scripts/Singleton/GameManager.cs
+++ Assets/Code/Scripts/Singleton/GameManager.cs
@@ -2,33 +2,76 @@ using System;
 using System.Collections.Generic;
 using Code.Scripts.Entities;
 using Code.Scripts.SO;
+using Code.Scripts.Types;
 using Code.Scripts.Utils;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Code.Scripts.Singleton
 {
     public class GameManager : SingletonMonoBehaviour<GameManager>
     {
         [Header("Game settings")]
+        [SerializeField] [Range(1, 5)] private int _maxPlayerChoices = 3;
         [SerializeField] private int _judgesMaxConviction;
 
         [Header("References")]
         [SerializeField] private GameObject _choicePrefab;
-        [SerializeField] private List<PlayerChoice> _choicePool;
+        [SerializeField] private List<PlayerChoiceSO> _choicePool = new ();
         [SerializeField] private RectTransform _playerChoicePanel;
 
         public int JudgesMaxConviction => _judgesMaxConviction;
 
-        private Judge[] _judges;
+        // Game Data
+        private GameMode _currentGameMode = GameMode.Trial;
+
+        // Events
+        public Action<PlayerChoiceSO> OnPlayerAction;
 
         private void Awake()
         {
+            DontDestroyOnLoad(gameObject);
             Judge.InitializeJudges();
         }
 
         private void Start()
         {
-            throw new NotImplementedException();
+            RefreshPlayerChoices();
+        }
+
+        private void Update()
+        {
+        }
+
+        // ----- //
+
+        private void RefreshPlayerChoices()
+        {
+            List<PlayerChoiceSO> so = PullPlayerChoiceSO();
+
+            for (int index = 0; index < _maxPlayerChoices; index++)
+            {
+                Choice playerChoice = Instantiate(_choicePrefab, _playerChoicePanel).GetComponent<Choice>();
+                PlayerChoiceSO choiceData = _choicePool[index];
+                playerChoice.Setup(choiceData);
+            }
+        }
+
+        private List<PlayerChoiceSO> PullPlayerChoiceSO()
+        {
+            List<PlayerChoiceSO> so = new(_maxPlayerChoices);
+
+            while (so.Count < _maxPlayerChoices)
+            {
+                PlayerChoiceSO soData = _choicePool[Random.Range(0, _choicePool.Count)];
+
+                if (so.Contains(soData))
+                    continue;
+
+                so.Add(soData);
+            }
+
+            return so;
         }
     }
 }
