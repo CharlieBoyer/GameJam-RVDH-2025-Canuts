fe27d58f1430f876055fe1a0cda7b28bc775a891
AbhorashBDs <valleixmartin89@gmail.com>
2025-10-06 17:34:42 +0200

    SFX Added + ausiomanager update

--- Assets/Code/Scripts/Audio/AudioClipsIndex.cs
+++ Assets/Code/Scripts/Audio/AudioClipsIndex.cs
@@ -8,14 +8,37 @@ namespace Code.Scripts.Audio
     public class AudioClipsIndex
     {
         // TODO: All audio clips of the project
-        [Header("UI")]
-        [field:SerializeField] public AudioClip UIButtonSelected { get; private set; }
-        [field:SerializeField] public AudioClip UIButtonHoverEnter { get; private set; }
-        [field:SerializeField] public AudioClip UIButtonHoverExit { get; private set; }
+        [Header("OusamaQuote")]
+        [field:SerializeField] public AudioClip Attack { get; private set; }
+        [field:SerializeField] public AudioClip Diplomacy { get; private set; }
+        [field:SerializeField] public AudioClip Dishonor { get; private set; }
+        [field: SerializeField] public AudioClip Injustice { get; private set; }
+        [field: SerializeField] public AudioClip Threat { get; private set; }
 
         [Header("Ambiance")]
         [field:SerializeField] public AudioClip MenuMusic { get; private set; }
         [field:SerializeField] public AudioClip GameMusic { get; private set; }
+
+        [Header("Victory/Defeat")]
+        [field: SerializeField] public AudioClip GameOver { get; private set; }
+        [field: SerializeField] public AudioClip GameWon { get; private set; }
+
+        [Header("Pleading")]
+        [field: SerializeField] public AudioClip PleadingEnd { get; private set; }
+        [field: SerializeField] public AudioClip PleadingTimer { get; private set; }
+        [field: SerializeField] public AudioClip PleadingStart { get; private set; }
+
+        [Header("Dialogue")]
+        [field: SerializeField] public AudioClip PassText { get; private set; }
+        [field: SerializeField] public AudioClip OusamaVoice { get; private set; }
+        [field: SerializeField] public AudioClip RenierVoice { get; private set; }
+
+        [Header("UI")]
+        [field: SerializeField] public AudioClip UIHover { get; private set; }
+        [field: SerializeField] public AudioClip UIReturn { get; private set; }
+        [field: SerializeField] public AudioClip UIValidation { get; private set; }
+
+
 
     }
 }
